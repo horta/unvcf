e03bc5ae86f9312ef760e77ce30e60616de4948f
Danilo Horta <danilo.horta@gmail.com>
2018-01-24 13:40:23 +0000

    simple markdown

--- unvcf/__init__.py
+++ unvcf/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import as _
 from ._cmd import unvcf
 from .testit import test
 
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 __all__ = ['__version__', 'unvcf', 'test']
