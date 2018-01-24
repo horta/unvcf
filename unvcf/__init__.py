78acc79593c896844fd6b80092d537bc005108d8
Danilo Horta <danilo.horta@gmail.com>
2018-01-24 14:02:44 +0000

    readme english

--- unvcf/__init__.py
+++ unvcf/__init__.py
@@ -3,6 +3,6 @@ from __future__ import absolute_import as _
 from ._cmd import unvcf
 from .testit import test
 
-__version__ = '0.1.2'
+__version__ = '0.1.3'
 
 __all__ = ['__version__', 'unvcf', 'test']
